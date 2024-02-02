a5db444cebd160267f559a2fff2d0c3706dfe2e1
DimitarGeorgiev93 <156999689+DimitarGeorgiev93@users.noreply.github.com>
2024-02-02 11:57:16 +0200

    Update main.cpp

--- CourierSystem/main.cpp
+++ CourierSystem/main.cpp
@@ -1,7 +1,6 @@
-
 #include <iostream>
 
-
+#include<vector>
 #include "ExpressPackage.h"
 
 int main()
@@ -9,9 +8,21 @@ int main()
     Adress a("Pencho Zlatev", "Sofiq", "Carigradsko shose", "123445");
     Adress b("Zlatko Penchev", "Varna", "Mosta za skachane", "987456");
 
-    Package pack(a,b,23,2);
-    ExpressPackage pack2(a,b,23,2,10);
-    pack.print();
-    pack2.print();
+    Package pack(a, b, 23, 2);
+    ExpressPackage pack2(a, b, 23, 2, 10);
+
+    std::vector<Package*> packages;
+    packages.push_back(&pack);
+    packages.push_back(&pack2);
+
+    packages.push_back(&pack2);
+    packages.push_back(&pack);
+    packages.push_back(&pack2);
+    packages.push_back(&pack);
+
+    for (Package* package : packages) {
+        std::cout << "----------------------" << std::endl;
+        package->print();
+    }
 }
 
