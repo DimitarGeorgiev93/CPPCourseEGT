b456e18eea4231fc6d18889beae77baea699ecd0
DimitarGeorgiev93 <156999689+DimitarGeorgiev93@users.noreply.github.com>
2024-02-15 14:38:10 +0200

    Add files via upload

---
+++ SDL_MovableImage_homework/Game.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+#include <SDL.h>
+#include <SDL_image.h> //png, jpeg, etc
+#include "TextureManager.h"
+#include "Shapes.h"
+
+
+
+class Game {
+public:
+	
+	
+	Game();
+	~Game();
+	bool init(const char* title, int xpos, int ypos, int width, int height, int flags);
+	void handleEvents();
+	void update();
+	//void update1(); // задача 1
+	void render(); 
+	void clean(); 
+
+	bool isRunning();
+private:
+	void limitFPS(Uint32);
+	int getRand() const;
+	SDL_Window* window = NULL;
+	SDL_Renderer* renderer = NULL;
+	bool running;
+
+	SDL_Texture* texture = NULL;
+	SDL_Rect sourceRectangle; 
+	SDL_Rect destinationRectangle; 
+
+	int currentFrame; 
+};
---
+++ SDL_MovableImage_homework/TextureManager.cpp
@@ -0,0 +1,62 @@
+#include "TextureManager.h"
+
+int TextureManager::x = 0;
+int TextureManager::y = 0;
+//int TextureManager::speed = 5; // starting speed
+//bool TextureManager::position = true;
+
+
+bool TextureManager::loadTexture(const char* fileName, std::string id, SDL_Renderer* ren) {
+	SDL_Surface* tempSurface = IMG_Load(fileName);
+	if (tempSurface == 0)
+		return false;  // if something went wrong
+	SDL_Texture* tex = SDL_CreateTextureFromSurface(ren, tempSurface);
+	SDL_FreeSurface(tempSurface);
+
+	if (tex != 0) {
+		textureMap[id] = tex;
+		//visibility[id] = false;
+		return true;
+	}
+
+	return false; // if something goes wrong
+}
+
+void TextureManager::drawTexture(std::string id, int x, int y, int width, int height, SDL_Renderer* ren, SDL_RendererFlip flip) 
+{
+	SDL_Rect srcRect;
+	SDL_Rect destRect;
+	srcRect.x = srcRect.y = 0;
+	srcRect.w = destRect.w = width;
+	srcRect.h = destRect.h = height;
+	destRect.x = x;
+	destRect.y = y;
+	SDL_RenderCopyEx(ren, textureMap[id], &srcRect, &destRect, 0, 0, flip);
+}
+
+
+
+//void TextureManager::toggleVisibility(std::string id)
+//{
+//	if (visibility[id] == true) {
+//		visibility[id] = false;
+//	}
+//	else visibility[id] = true;
+//}
+
+
+//void TextureManager::drawOneFrameFromTexture(std::string id, int x, int y,int width, int height, int currentRow, int currentFrame,SDL_Renderer* ren,SDL_RendererFlip flip)
+//{
+//	SDL_Rect srcRect;
+//	SDL_Rect destRect;
+//	srcRect.x = width * currentFrame;
+//	srcRect.y = height * (currentRow - 1);
+//	srcRect.w = destRect.w = width;
+//	srcRect.h = destRect.h = height;
+//	destRect.x = x;
+//	destRect.y = y;
+//
+//	SDL_RenderCopyEx(ren, textureMap[id], &srcRect, &destRect, 0, 0, flip);
+//}
+
+TextureManager* TextureManager::instance = 0;
